b0e82dbf6c5b084307fb1a7bd22145ce602da94c
harrychen <harry@hymarr.com>
2015-01-09 11:48:41 -0800

    customize top bar

---
+++ Jeanine_IOS/MainNavigationBar.swift
@@ -0,0 +1,21 @@
+//
+//  MainNavigationBar.swift
+//  Jeanine_IOS
+//
+//  Created by Jianzhong Chen on 1/9/15.
+//  Copyright (c) 2015 Jianzhong Chen. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationBar: UINavigationBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
