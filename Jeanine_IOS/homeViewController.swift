fa5dfa4658ef6c86a468042531829af453d0c6a9
Qihan Zhu <a7hv587@Qihans-MacBook-Pro.local>
2015-01-10 10:48:14 -0800

    add table

--- Jeanine_IOS/homeViewController.swift
+++ Jeanine_IOS/homeViewController.swift
@@ -8,15 +8,46 @@
 
 import UIKit
 
-class homeViewController: UIViewController, UITableViewDelegate {
-
+class homeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var sideMenu: UITableView!
+    @IBOutlet weak var myTableView: UITableView!
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return targets.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let target = targets[indexPath.row]
+        let cell: homeTableViewCell = tableView.dequeueReusableCellWithIdentifier("homeCell") as homeTableViewCell
+        cell.update(target.name, image:target.image, time: target.time, like: target.like, comment: target.comment)
+        return cell
+    }
+
+    var targets:[homeTarget] = [homeTarget]()
+    
+    func setup_targets() {
+        var t1 = homeTarget(type:0, name: "Qihan", image: "img1", words: "", time: "3h", like: 321, comment: 25)
+        var t2 = homeTarget(type:0, name: "jianzhong", image: "img2", words: "", time: "7h", like: 123, comment: 21)
+        var t3 = homeTarget(type: 1, name: "chi", image: "img3", words: "This design is awesome!", time: "1d", like: 213, comment: 5)
+        targets.append(t1)
+        targets.append(t2)
+        targets.append(t3)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        sideMenu.delegate = self
         // Do any additional setup after loading the view.
+        
+//        self.myTableView.autoresizingMask =  UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+        
+        self.setup_targets()
+        self.myTableView.dataSource = self
+        self.myTableView.delegate = self
+        
     }
     
     override func didReceiveMemoryWarning() {
