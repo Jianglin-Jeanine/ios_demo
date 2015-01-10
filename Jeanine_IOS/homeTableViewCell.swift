29724f3c3d977fbdc4e81798af844f6f1b3372f1
Qihan Zhu <a7hv587@Qihans-MacBook-Pro.local>
2015-01-10 10:12:02 -0800

    add table

---
+++ Jeanine_IOS/homeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  homeTableViewCell.swift
+//  Jeanine_IOS
+//
+//  Created by Qihan Zhu on 1/9/15.
+//  Copyright (c) 2015 Jianzhong Chen. All rights reserved.
+//
+
+import UIKit
+
+class homeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
